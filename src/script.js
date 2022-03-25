e65b76fe85c58be372d4521252a9bdbfb93d3666
Razvan <razvanbugoi@gmail.com>
2022-03-25 17:00:18 +0000

    working on toggling the book read status

--- src/script.js
+++ src/script.js
@@ -1,4 +1,19 @@
 let myLibrary = [];
+let removeIcons;
+let counter = 0;
+let booksStatus;
+
+const startLibraryButton = document.getElementById('start-library')
+const formContainer = document.getElementsByClassName('form-container')
+const closeButton = document.getElementById('close-button')
+const submitButton = document.getElementById('submit-button')
+
+// Input Values 
+const bookTitle = document.getElementById('title')
+const bookAuthor = document.getElementById('author')
+const bookPages = document.getElementById('pages')
+const bookStatus = document.getElementById('checkbox')
+
 
 function Book(title, author, noOfPages, status) {
   this.title = title
@@ -12,23 +27,21 @@ function Book(title, author, noOfPages, status) {
 }
 
 
-let removeIcons;
-
-
-
-
 function addBookToLibrary(counter) {
   const newTr = document.createElement("tr")
   const newTdTitle = document.createElement("td")
   const newTdAuthor = document.createElement("td")
   const newTdPages = document.createElement("td")
   const newTdStatus = document.createElement("td")
+  newTdStatus.className = "status"
+  newTdStatus.id = `book-status${counter}`
   const newTdRemove = document.createElement("td")
   const removeImage = new Image()
   removeImage.src = "../assets/trash-can-outline.png"
   removeImage.alt = "Remove Book Icon"
   removeImage.className = "remove-icons"
   removeImage.id = `remove-icon${counter}`
+
 
   newTdTitle.textContent = `${myLibrary[myLibrary.length - 1].title}`
   newTdAuthor.textContent = `${myLibrary[myLibrary.length - 1].author}`
@@ -48,29 +61,21 @@ function addBookToLibrary(counter) {
 
   for (let i=0; i<removeIcons.length; i++) {
     removeIcons[i].onclick = () => {
-    console.log(removeIcons[i].id)
     myLibrary.splice(`${removeIcons[i]}`, 1)
     let bookId = removeIcons[i].id
     const removeTr = document.getElementById(`${bookId}`)
     removeBook(removeTr)
     }
   }
+
+  booksStatus = document.querySelectorAll(".status")
+  toggleReadStatus(newTdStatus)
 }
 
 function removeBook(book) {
   book.parentElement.remove()
 }
 
-const startLibraryButton = document.getElementById('start-library')
-const formContainer = document.getElementsByClassName('form-container')
-const closeButton = document.getElementById('close-button')
-const submitButton = document.getElementById('submit-button')
-
-// Input Values 
-const bookTitle = document.getElementById('title')
-const bookAuthor = document.getElementById('author')
-const bookPages = document.getElementById('pages')
-const bookStatus = document.getElementById('checkbox')
 
 startLibraryButton.onclick = () => {
   formContainer[0].style.display = 'block';
@@ -81,7 +86,6 @@ closeButton.onclick = () => {
 }
 
 
-let counter = 0;
 submitButton.onclick = (event) => {
   event.preventDefault();
   
@@ -90,8 +94,27 @@ submitButton.onclick = (event) => {
   myLibrary.push(book);
   counter = myLibrary.indexOf(book)
 
-  // counter ++
   console.log(counter)
 
   addBookToLibrary(counter)
+}
+
+function toggleReadStatus(newTdStatus) {
+  booksStatus.forEach((element) => {
+    element.onclick = () => {
+      const currentCounter = element.id.slice(-1)
+      console.log(currentCounter)
+      console.log(newTdStatus)
+      newTdStatus.id.replace(/\d/g, currentCounter);
+      if (myLibrary[currentCounter].status == "true") {
+        myLibrary[currentCounter].status = "false"
+        console.log(newTdStatus.id)
+        newTdStatus.textContent = "false"
+      } else {
+        myLibrary[currentCounter].status = "true"
+        console.log(newTdStatus.id)
+        newTdStatus.textContent = "true"
+      }
+    }
+  })
 }
